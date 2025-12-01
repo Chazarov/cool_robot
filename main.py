7477aafdf1037c9dfdf02b0f31e194b2eeb4d9ea
Chazarow <zaharowanrew@gmail.com>
2025-12-01 11:12:32 +0400

    add main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from analyse_service import merge_transcription_diarization
+
+if __name__ == "__main__":
+    dialogue = merge_transcription_diarization("examples/e1.mp3", n_speakers=5)
+    
+    for speaker, text in dialogue:
+        print(f"{speaker}: {text}")
+
